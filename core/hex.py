bb0455fafd9a3f2ffc9849fac89a6b6534607fe2
Rodrigo Fischer <rfischeracc@gmail.com>
2021-05-15 20:12:01 -0300

    [added interface to play with AI]

--- core/hex.py
+++ core/hex.py
@@ -1,7 +1,5 @@
 import numpy as np
 from collections import defaultdict
-from itertools import cycle
-import sys
 import pygame as pg
 import pygame.locals as pl
 
@@ -112,11 +110,11 @@ class HexTextInterface:
             print(k * ' ', " ".join(str(x) if x != 0 else "." for x in line))
 
 
-class HexGUI:
+class HexPVPGUI:
     def __init__(self, x, y, size):
         self.x, self.y = x, y
         self.game = HexBoard(x, y)
-        self.player_cycle = cycle([1, 2])
+        self.player_no = 0
         self.square_size = size
         self.rects = dict()
         # Pygame stuff:
@@ -158,28 +156,133 @@ class HexGUI:
             self.FPS.tick(15)
             for event in pg.event.get():
                 if event.type == pl.QUIT:
+                    pg.display.quit()
+                    pg.quit()
                     return
-                if event.type == pl.MOUSEBUTTONDOWN:
+
+                if event.type == pl.MOUSEBUTTONDOWN and self.game.winner is None:
                     self.mouse_callback(pg.mouse.get_pos())
 
     def mouse_callback(self, pos):
         for (i, j), rect in self.rects.items():
             if rect.collidepoint(pos):
-                current_player_no = next(self.player_cycle)
+                current_player_no = self.player_no + 1
                 self.game.play(current_player_no, i + 1, j + 1)
-                pg.draw.rect(self.display, colors[current_player_no], rect)
-                pg.display.update()
-                # self.show()
-                if self.game.winner is not None:
-                    self.cleanup()
+                if self.game.crash:
+                    print(f"Invalid move, please play again player {self.player_no + 1}")
+                    self.game.crash = False
+
+                else:
+                    self.player_no += 1
+                    self.player_no %= 2
+                    pg.draw.rect(self.display, colors[current_player_no], rect)
+                    pg.display.update()
+                    if self.game.winner is not None:
+                        self.cleanup()
                 return
 
     def cleanup(self):
-        print(f"{self.game.winner} won")
-        sys.exit()
+        print(f"Player {self.game.winner} won!")
+
+
+class HexAIGUI:
+    def __init__(self, x, y, size, ai):
+        self.x, self.y = x, y
+        self.ai = ai
+        self.game = HexBoard(x, y)
+        self.endgame = False
+        self.square_size = size
+        self.rects = dict()
+        # Pygame stuff:
+        pg.init()
+        self.FPS = pg.time.Clock()
+        for i in range(y):
+            for j in range(x):
+                self.rects[j, i] = pg.Rect(
+                    (1.1 * (2 * i + j) + 2) * self.square_size / 2,  # x pos
+                    (1.1 * j + 1) * self.square_size,  # y pos
+                    self.square_size,  # width
+                    self.square_size)  # height
+        self.display = pg.display.set_mode((
+            int(1.1 * (2 * x + y + 2) * self.square_size / 2),
+            int(1.1 * (y + 1) * self.square_size)
+        ))
+        self.display.fill((255, 255, 255))
+        pg.display.update()
+
+    def show(self):
+        for k, line in enumerate(self.game.board):
+            print(k * ' ', " ".join(str(x) if x != 0 else "." for x in line))
+
+    def draw(self):
+        top_left = (0.4 * self.square_size, 0.9 * self.square_size)
+        top_right = ((1.1 * self.x + 1) * self.square_size, 0.9 * self.square_size)
+        bottom_left = ((0.4 + 1.1 * self.y / 2) * self.square_size, (1.1 * self.y + 1) * self.square_size)
+        bottom_right = ((0.9 + 1.1 * (self.y / 2 + self.x)) * self.square_size, (1.1 * self.y + 1) * self.square_size)
+        pg.draw.line(self.display, colors[1], top_left, top_right, 5)
+        pg.draw.line(self.display, colors[1], bottom_left, bottom_right, 5)
+        pg.draw.line(self.display, colors[2], top_left, bottom_left, 5)
+        pg.draw.line(self.display, colors[2], top_right, bottom_right, 5)
+        for rect in self.rects.values():
+            pg.draw.rect(self.display, [100, 100, 100], rect)
+        pg.display.update()
+
+    def loop(self):
+
+        # AI plays first
+        self.ai_turn()
+
+        while True:
+            self.FPS.tick(15)
+            for event in pg.event.get():
+                if event.type == pl.QUIT:
+                    pg.display.quit()
+                    pg.quit()
+                    return
+
+                if event.type == pl.MOUSEBUTTONDOWN and not self.endgame:
+                    self.mouse_callback(pg.mouse.get_pos())
+
+    def mouse_callback(self, pos):
+        for (i, j), rect in self.rects.items():
+            if rect.collidepoint(pos):
+                # User plays
+                move = (i + 1, j + 1)
+                self.game.play(2, *move)
+                if self.game.crash:
+                    print(f"Invalid move, please play again player {2}")
+                    self.game.crash = False
+
+                else:
+                    pg.draw.rect(self.display, colors[2], self.rects[(i, j)])
+                    pg.display.update()
+                    if self.game.winner is not None:
+                        self.endgame = True
+                        print(f"Player {self.game.winner} won!")
+
+                    else:
+                        self.ai_turn()
+
+                return
+
+    def ai_turn(self):
+        # AI plays
+        state = self.game.get_state()
+        move = self.game.get_xy(np.argmax(self.ai(state)))
+        self.game.play(1, *move)
+        if self.game.crash:
+            print(f"AI has made a mistake!")
+            self.endgame = True
+
+        else:
+            pg.draw.rect(self.display, colors[1], self.rects[(move[0] - 1, move[1] - 1)])
+            pg.display.update()
+            if self.game.winner is not None:
+                self.endgame = True
+                print(f"Player {self.game.winner} won!")
 
 
 # To run the GUI, execute the following code
-# game = HexGUI(7, 7, 50)
+# game = HexPVPGUI(7, 7, 50)
 # game.draw()
 # game.loop()
