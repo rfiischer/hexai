33a36d562ccb7eda8452290370127e5de5d7ecd0
Rodrigo Fischer <rodrfischer@hotmail.com>
2021-05-17 13:59:50 -0300

    [trainable version of hex_script_0]

--- scripts/hex/hex_script_0.py
+++ scripts/hex/hex_script_0.py
@@ -1,6 +1,7 @@
 from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
+from scipy.io import savemat
 
 from core.hex import HexBoard
 from core.dqn import DQNAgent
@@ -25,20 +26,21 @@ c1_filters = 9
 c1_kernel = 2
 
 # Learning parameters
-num_episodes = 1000
-explore_episodes = 250
+num_episodes = 30000
+explore_episodes = 10000
 e_max = 1
 e_min = 0.05
-epsilon_array = np.concatenate((epsilon_function(e_max, e_min, 750, explore_episodes),
-                                [e_min] * 250))
-reward = 10
-punishment = -10
+epsilon_array = np.concatenate((epsilon_function(e_max, e_min, explore_episodes, explore_episodes),
+                                [e_min] * 20000))
+reward = 1
+punishment = -1
 
 # Build base model
 base_model = keras.Sequential(
     [
-        layers.InputLayer(input_shape=(board_size, board_size, 1)),
+        layers.InputLayer(input_shape=state_shape),
         layers.Conv2D(c1_filters, c1_kernel, activation="relu", name="c1", bias_initializer="normal"),
+        layers.Conv2D(c1_filters, c1_kernel, activation="relu", name="c2", bias_initializer="normal"),
         layers.Flatten(),
         layers.Dense(grid_size, activation="linear", name="output")
     ]
@@ -61,8 +63,6 @@ agent = DQNAgent(base_model, base_weights, dqn_params)
 
 # Run episodes
 # In this script, we reward for completing the game both in winning and losing scenarios
-num_wins = 0
-num_loose = 0
 num_crash = 0
 for episode in range(num_episodes):
 
@@ -71,8 +71,9 @@ for episode in range(num_episodes):
     # Perform epsilon-greedy action
     epsilon = epsilon_array[episode]
 
-    # Opponent beginns by choosing random legal move
-    board.play(2, *board.get_xy(np.random.choice(board.get_legal())))
+    # Opponent beginns randomly
+    if np.random.randint(0, 2):
+        board.play(2, *board.get_xy(np.random.choice(board.get_legal())))
 
     finished = False
     number_moves = 0
@@ -96,36 +97,30 @@ for episode in range(num_episodes):
             agent.update_memory(transition)
             num_crash += 1
 
-        # Main agent wins
-        elif board.winner is not None:
+        elif not np.any(board.get_legal()):
             finished = True
-            transition = [start_state, move, reward, start_state, True]
+            end_state = board.get_state()
+            transition = [start_state, move, reward, end_state, True]
             agent.update_memory(transition)
-            num_wins += 1
 
-        # No winner yet (second agent plays)
+        # Continue playing (no matter if anyone has already won)
         else:
             # Choose random legal move
             board.play(2, *board.get_xy(np.random.choice(board.get_legal())))
 
-            # See if opponent wins
-            if board.winner is not None:
+            if not np.any(board.get_legal()):
                 finished = True
-                transition = [start_state, move, reward, start_state, True]
-                agent.update_memory(transition)
-                num_loose += 1
 
-            else:
-                end_state = board.get_state()
-                transition = [start_state, move, reward, end_state, False]
-                agent.update_memory(transition)
+            end_state = board.get_state()
+            transition = [start_state, move, reward, end_state, finished]
+            agent.update_memory(transition)
 
         agent.train()
 
         number_moves += 1
 
-    print(f"Episode: {episode}, Moves: {number_moves}, Win: {num_wins}, Crash: {num_crash},"
-          f"Lose: {num_loose}, Loss: {agent.callback.loss[-1]}")
+    print(f"Episode: {episode}, Moves: {number_moves}, Crash: {num_crash}, Loss: {agent.callback.loss[-1]}")
 
 # Save model
 keras.models.save_model(agent.online, "./hmodel0")
+savemat('./hmodel0/loss.mat', {'loss': loss})
